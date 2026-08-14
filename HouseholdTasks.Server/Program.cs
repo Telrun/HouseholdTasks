c9188baf821d777544d29a604c59ea1e3f7695c2
Rune Tellefsen <rune.tellefsen@furst.no>
2026-08-14 07:49:48 +0200

    Oppretter tasks.s23.no

--- HouseholdTasks.Server/Program.cs
+++ HouseholdTasks.Server/Program.cs
@@ -87,7 +87,7 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
         ForwardedHeaders.XForwardedFor |
         ForwardedHeaders.XForwardedProto;
 
-    options.KnownNetworks.Clear();
+    options.KnownIPNetworks.Clear();
     options.KnownProxies.Clear();
 });
 
